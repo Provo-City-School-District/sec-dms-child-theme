96d8f4147000821143af52612e804c18d011a6d7
Espi <joshespi@gmail.com>
2021-11-24 11:26:27 -0700

    added requested links to the policies and forms menu.
    -converted docs to PDFs where applicable
    -removed the Procedures heading and put the Forms heading in the center column
    -seperated the forms menu into two columns so that the mega menu is balanced

--- dms-child/template-parts/mega-menu-dropdowns-policies.php
+++ dms-child/template-parts/mega-menu-dropdowns-policies.php
@@ -1,4 +1,4 @@
-<div class="dropDownColumn">
+				<div class="dropDownColumn">
 					<?php
 						//checks if this template section is loading into the menu or into the sidebar/content area and assigns the proper heading
 						if(is_page( 'policies-forms' ) || in_array( 43027, get_post_ancestors($post))){
@@ -11,28 +11,11 @@
 							<?php
 						}
 					?>
-
-						<ul>
-							<li><a href="https://dixon.provo.edu/wp-content/uploads/2021/07/Dixon-Student-Handbook-21-22.pdf">Student Handbook</a></li>
-						</ul>
-				</div>
-				<div class="dropDownColumn">
-					<?php
-						//checks if this template section is loading into the menu or into the sidebar/content area and assigns the proper heading
-						if(is_page( 'policies-forms' ) || in_array( 43027, get_post_ancestors($post))){
-							?>
-							<h2><img src="https://dixon.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/procedures1.svg" alt="" />Procedures</h2>
-							<?php
-						} else {
-							?>
-							<h3><img src="https://dixon.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/procedures1.svg" alt="" />Procedures</h3>
-							<?php
-						}
-					?>
-
-						<ul>
-
-						</ul>
+					<ul>
+						<li><a href="https://dixon.provo.edu/wp-content/uploads/2021/07/Dixon-Student-Handbook-21-22.pdf">Student Handbook</a></li>
+						<li><a href="https://dixon.provo.edu/policies-forms/after-school-program-rules-and-policies/">After School Program Rules and Policies</a></li>
+						<li><a href="https://dixon.provo.edu/policies-forms/dress-grooming-standards/">Dress and Grooming Standards</a></li>
+					</ul>
 				</div>
 				<div class="dropDownColumn">
 					<?php
@@ -47,9 +30,19 @@
 							<?php
 						}
 					?>
+					<ul>
+						<li><a href="https://www.myschoolapps.com/">Application for Free and Reduced Meals</a></li>
+						<li><a href="https://drive.google.com/file/d/0B-HbeleIpshoNndWcDlCNExBNW8/edit?pli=1&resourcekey=0-TnvDMZW40-jX4eAKbKel6g">Summer School Registration Form</a></li>
+						<li><a href="https://dixon.provo.edu/wp-content/uploads/2014/12/SCC-Nomination-Form-for-Election-2016-2017.pdf">School Community Council Candidate Form</a></li>
+						<li><a href="http://dixon.provo.edu/wp-content/uploads/2021/11/Job-Shadow-Agreement.pdf">Job Shadow Agreement Form</a></li>
 
-						<ul>
+					</ul>
 
-
-						</ul>
+				</div>
+				<div class="dropDownColumn">
+					<ul>
+						<li><a href="https://dixon.provo.edu/wp-content/uploads/2021/02/2021-2022-Immunization-Requirements.pdf">2021-2022 Immunization Requirements Form</a></li>
+						<li><a href="https://dixon.provo.edu/wp-content/uploads/2021/02/2021-2022-Vacunas.pdf">2021-2022 Immunization Requirements Form (Spanish)</a></li>
+						<li><a href="http://dixon.provo.edu/wp-content/uploads/2015/02/Residency-Questionnaire.pdf">Student Residency Questionnaire</a></li>
+					</ul>
 				</div>
