728e04cc78e8858136fef35b7f04ce5c67085934
Espi <joshespi@gmail.com>
2021-11-24 15:33:35 -0700

    added document links to the counseling menu

--- dms-child/template-parts/mega-menu-dropdowns-counseling.php
+++ dms-child/template-parts/mega-menu-dropdowns-counseling.php
@@ -3,25 +3,25 @@
 		//checks if this template section is loading into the menu or into the sidebar/content area and assigns the proper heading
 		if(is_page( 'counseling-registration/' ) || in_array( 42312, get_post_ancestors($post))){
 			?>
-			<h2><img src="https://dixon.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/folded-newspaper.svg" alt="" />Counseling News</h2>
+			<h2><img src="https://dixon.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/folded-newspaper.svg" alt="" />Counseling</h2>
 			<?php
 		} else {
 			?>
-			<h3><img src="https://dixon.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/folded-newspaper.svg" alt="" />Counseling News</h3>
+			<h3><img src="https://dixon.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/folded-newspaper.svg" alt="" />Counseling</h3>
 			<?php
 		}
 	?>
-
-		<ul>
-
-		</ul>
+	<ul>
+		<li><a href="https://dixon.provo.edu/couseling-registration/counseling-center/">Counseling Center</a></li>
+		<li><a href="">Find your Counselor</a></li>
+	</ul>
 </div>
 
 <div class="dropDownColumn">
 	<?php
 		//checks if this template section is loading into the menu or into the sidebar/content area and assigns the proper heading
 		if(is_page( 'counseling-registration/' ) || in_array( 42312, get_post_ancestors($post))){
-			?>
+			?>/Volumes/MacOS/Users/joshe-work/Library/Caches/Nova/88EE6ACF-3032-4305-881C-A8A835EDD624/158.91.4.26/var/www/html/dms-redux/wp-content/themes/dms-child/template-parts/mega-menu-dropdowns-counseling.php
 			<h2><img src="https://dixon.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/registration.svg" alt="" />Registration</h2>
 			<?php
 		} else {
@@ -30,26 +30,43 @@
 			<?php
 		}
 	?>
-
 	<ul>
-
+		<li><a href="">Registration Information</a></li>
+		<li><a href="http://dixon.provo.edu/wp-content/uploads/2021/02/2021-2022-Dixon-Course-Description-.pdf">2021-2022 Dixon Course Description</a></li>
+		<li><a href="http://dixon.provo.edu/wp-content/uploads/2021/02/2021-2022-8th-Grade-Registration-Instructions.pdf">2021-2022 8th Grade Registration Instructions</a></li>
+		<li><a href="http://dixon.provo.edu/wp-content/uploads/2021/02/2021-2022-8vo-Grado-Instrucciones-para-Inscripcion.pdf">2021-2022 8vo Grado Instrucciones para Inscripción</a></li>
+		<li><a href="http://dixon.provo.edu/wp-content/uploads/2021/03/2021-2022-7th-Grade-Registration-Instructions.pdf">2021-2022 7th Grade Registration Instructions</a></li>
+		<li><a href="http://dixon.provo.edu/wp-content/uploads/2021/03/2021-2022-Spanish-7th-Grade-Registration-Instructions.pdf">2021-2022 Spanish 7th Grade Registration Instructions</a></li>
+		<li><a href="">2021-2022 Registration FAQ</a></li>
+		<li><a href="http://dixon.provo.edu/wp-content/uploads/2021/03/2021-2022-Additional-Information.pdf">2021-2022 Additional Information</a></li>
+		<li><a href="http://dixon.provo.edu/wp-content/uploads/2021/03/2021-2022-Principals-Letter-for-6th-Grade-Students-and-Parents.pdf">2021-2022 Principal's Letter for 6th Grade Students and Parents</a></li>
+		<li><a href="http://dixon.provo.edu/wp-content/uploads/2021/03/2021-2022-Spanish-Principals-Letter-for-6th-Grade-Students-and-Parents.pdf">2021-2022 Spanish Principal's Letter for 6th Grade Students and Parents</a></li>
+		<li><a href="http://dixon.provo.edu/wp-content/uploads/2021/02/2021-2022-Immunization-Requirements.pdf">2021-2022 Immunization Requirements</a></li>
+		<li><a href="http://dixon.provo.edu/wp-content/uploads/2021/02/2021-2022-Vacunas.pdf">2021-2022 Immunization Requirements Form (Spanish)</a></li>
+		<li><a href="http://dixon.provo.edu/wp-content/uploads/2015/02/Residency-Questionnaire.pdf">Student Residency Questionnaire</a></li>
+		<li><a href="https://docs.google.com/forms/d/e/1FAIpQLSdEL_r30yB7v9OsfQNZJcspChBFOG2pE1d2K9zLfiAtkC-Vrg/viewform">Summer School Registration Form</a></li>
 	</ul>
 </div>
+
 <div class="dropDownColumn">
 	<?php
 		//checks if this template section is loading into the menu or into the sidebar/content area and assigns the proper heading
 		if(is_page( 'counseling-registration/' ) || in_array( 42312, get_post_ancestors($post))){
 			?>
-			<h2><img src="https://dixon.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/planning.svg" alt="" />College &amp; Career Planning</h2>
+			<h2><img src="https://dixon.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/planning.svg" alt="" />Fees</h2>
 			<?php
 		} else {
 			?>
-			<h3><img src="https://dixon.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/planning.svg" alt="" />College &amp; Career Planning</h3>
+			<h3><img src="https://dixon.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/planning.svg" alt="" />Fees</h3>
 			<?php
 		}
 	?>
-
-		<ul>
-
-		</ul>
+	<ul>
+		<li><a href="https://provo.edu/school-fees/dixon-middle/">Fee Schedule</a></li>
+		<li><a>School Fees Notice</a></li>
+		<li><a>School Fees Notice (Spanish)</a></li>
+		<li><a href="https://provo.edu/wp-content/uploads/2021/07/6160-FeeWaiverApplication7-12.pdf"><span class="hidden">6160 Form 1 </span>Fee Waiver Application</a></li>
+		<li><a href="https://provo.edu/wp-content/uploads/2021/07/6160-FeeWaiverApplication7-12Spanish.pdf"><span class="hidden">6160 </span>Solicitud de Exoneracion de Cuotas</a></li>
+		<li><a href="https://provo.aliohost.net:7443/AlioFeePay_PSD/Account/Login">Pay School Fees</a></li>
+	</ul>
 </div>
--- dms-child/template-parts/mega-menu-dropdowns-staff.php
+++ dms-child/template-parts/mega-menu-dropdowns-staff.php
@@ -1,17 +1,17 @@
 <div class="dropDownColumn">
-	<?php 
+	<?php
 		//checks if this template section is loading into the menu or into the sidebar/content area and assigns the proper heading
 		if(is_page( 'faculty-staff' ) || in_array( 42310, get_post_ancestors($post))){
 			?>
 			<h2><img src="https://dixon.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/school-info.svg" alt="" />Teachers</h2>
-			<?php	
+			<?php
 		} else {
 			?>
 			<h3><img src="https://dixon.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/school-info.svg" alt="" />Teachers</h3>
 			<?php
-		}	
+		}
 	?>
-	
+
 		<ul>
 			<li class="int"><a href="https://dixon.provo.edu/our-teachers-staff/teacher-course-directory/">Find Your Teacher</a></li>
 			<li class="int"><a href="https://canvas.provo.edu">Canvas Login</a></li>
@@ -20,19 +20,19 @@
 		</ul>
 </div>
 <div class="dropDownColumn">
-	<?php 
+	<?php
 		//checks if this template section is loading into the menu or into the sidebar/content area and assigns the proper heading
 		if(is_page( 'faculty-staff' ) || in_array( 42310, get_post_ancestors($post))){
 			?>
 			<h2><img src="https://dixon.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/school-info.svg" alt="" />Staff</h2>
-			<?php	
+			<?php
 		} else {
 			?>
 			<h3><img src="https://dixon.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/school-info.svg" alt="" />Staff</h3>
 			<?php
-		}	
+		}
 	?>
-	
+
 		<ul>
 			<li class="int"><a href="https://dixon.provo.edu/our-teachers-staff/main-office-administration/">Administration &amp; Main Office</a></li>
 			<li class="int"><a href="https://dixon.provo.edu/counseling/find-your-counselor/">Find Your Counselor</a></li>
