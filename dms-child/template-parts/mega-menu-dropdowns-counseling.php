73d78469bff79fb06ba56663a69b818252b45fc2
Espi <joshespi@gmail.com>
2022-02-17 11:02:46 -0700

    removed outdated document links as requested

--- dms-child/template-parts/mega-menu-dropdowns-counseling.php
+++ dms-child/template-parts/mega-menu-dropdowns-counseling.php
@@ -15,6 +15,33 @@
 		<li><a href="<?php echo get_home_url(); ?>/counseling-registration/counseling-center/">Counseling Center</a></li>
 		<li><a href="<?php echo get_home_url(); ?>/counseling-registration/find-your-counselor/">Find your Counselor</a></li>
 	</ul>
+
+</div>
+
+<div class="dropDownColumn">
+	<?php
+		//checks if this template section is loading into the menu or into the sidebar/content area and assigns the proper heading
+		if(is_page( 'counseling-registration' ) || in_array( 18379, get_post_ancestors($post))){
+			?>
+			<h2><img src="https://dixon.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/registration.svg" alt="" />Registration</h2>
+			<?php
+		} else {
+			?>
+			<h3><img src="https://dixon.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/registration.svg" alt="" />Registration</h3>
+			<?php
+		}
+	?>
+	<ul>
+		<li><a href="http://dixon.provo.edu/wp-content/uploads/2022/02/22-23-Dixon-Course-Description-.pdf">2022-2023 Dixon Course Description</a></li>
+		<li><a href="http://dixon.provo.edu/wp-content/uploads/2022/02/22-23-8th-Registration-Instructions-English.pdf">2022-2023 8th Grade Registration Instructions (English)</a></li>
+		<li><a href="http://dixon.provo.edu/wp-content/uploads/2022/02/22-23-8th-Registration-Instructions-Spanish.pdf">2022-2023 8th Grade Registration Instructions (Spanish)</a></li>
+		<li><a href="http://dixon.provo.edu/wp-content/uploads/2022/02/2022-2023-English-Utah-Immunization-Requirements-for-7th-Grade-.pdf">2022-2023 English Utah Immunization Requirements for 7th Grade</a></li>
+		<li><a href="http://dixon.provo.edu/wp-content/uploads/2022/02/2022-2023-Spanish-Utah-Immunization-Requirements-for-7th-Grade-.pdf">2022-2023 Spanish Utah Immunization Requirements for 7th Grade</a></li>
+		<li><a href="https://docs.google.com/forms/d/e/1FAIpQLSdEL_r30yB7v9OsfQNZJcspChBFOG2pE1d2K9zLfiAtkC-Vrg/viewform">Summer School Registration Form</a></li>
+	</ul>
+</div>
+
+<div class="dropDownColumn noheading">
 	<?php
 		//checks if this template section is loading into the menu or into the sidebar/content area and assigns the proper heading
 		if(is_page( 'counseling-registration' ) || in_array( 18379, get_post_ancestors($post))){
@@ -34,41 +61,5 @@
 		<li><a href="https://provo.edu/wp-content/uploads/2021/07/6160-FeeWaiverApplication7-12.pdf"><span class="hidden">6160 Form 1 </span>Fee Waiver Application</a></li>
 		<li><a href="https://provo.edu/wp-content/uploads/2021/07/6160-FeeWaiverApplication7-12Spanish.pdf"><span class="hidden">6160 </span>Solicitud de Exoneracion de Cuotas</a></li>
 		<li><a href="https://provo.aliohost.net:7443/AlioFeePay_PSD/Account/Login">Pay School Fees</a></li>
-	</ul>
-</div>
-
-<div class="dropDownColumn">
-	<?php
-		//checks if this template section is loading into the menu or into the sidebar/content area and assigns the proper heading
-		if(is_page( 'counseling-registration' ) || in_array( 18379, get_post_ancestors($post))){
-			?>
-			<h2><img src="https://dixon.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/registration.svg" alt="" />Registration</h2>
-			<?php
-		} else {
-			?>
-			<h3><img src="https://dixon.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/registration.svg" alt="" />Registration</h3>
-			<?php
-		}
-	?>
-	<ul>
-		<li><a href="<?php echo get_home_url(); ?>/counseling-registration/registration-information/">Registration Information</a></li>
-		<li><a href="http://dixon.provo.edu/wp-content/uploads/2021/02/2021-2022-Dixon-Course-Description-.pdf">2021-2022 Dixon Course Description</a></li>
-		<li><a href="http://dixon.provo.edu/wp-content/uploads/2021/02/2021-2022-8th-Grade-Registration-Instructions.pdf">2021-2022 8th Grade Registration Instructions</a></li>
-		<li><a href="http://dixon.provo.edu/wp-content/uploads/2021/02/2021-2022-8vo-Grado-Instrucciones-para-Inscripcion.pdf">2021-2022 8vo Grado Instrucciones para Inscripción</a></li>
-		<li><a href="http://dixon.provo.edu/wp-content/uploads/2021/03/2021-2022-7th-Grade-Registration-Instructions.pdf">2021-2022 7th Grade Registration Instructions</a></li>
-		<li><a href="http://dixon.provo.edu/wp-content/uploads/2021/03/2021-2022-Spanish-7th-Grade-Registration-Instructions.pdf">2021-2022 Spanish 7th Grade Registration Instructions</a></li>
-		<li><a href="<?php echo get_home_url(); ?>/counseling-registration/2021-2022-registration-faq/">2021-2022 Registration FAQ</a></li>
-
-	</ul>
-</div>
-
-<div class="dropDownColumn noheading">
-	<ul>
-		<li><a href="http://dixon.provo.edu/wp-content/uploads/2021/03/2021-2022-Principals-Letter-for-6th-Grade-Students-and-Parents.pdf">2021-2022 Principal's Letter for 6th Grade Students and Parents</a></li>
-		<li><a href="http://dixon.provo.edu/wp-content/uploads/2021/03/2021-2022-Spanish-Principals-Letter-for-6th-Grade-Students-and-Parents.pdf">2021-2022 Spanish Principal's Letter for 6th Grade Students and Parents</a></li>
-		<li><a href="http://dixon.provo.edu/wp-content/uploads/2021/02/2021-2022-Immunization-Requirements.pdf">2021-2022 Immunization Requirements</a></li>
-		<li><a href="http://dixon.provo.edu/wp-content/uploads/2021/02/2021-2022-Vacunas.pdf">2021-2022 Immunization Requirements Form (Spanish)</a></li>
-		<li><a href="http://dixon.provo.edu/wp-content/uploads/2015/02/Residency-Questionnaire.pdf">Student Residency Questionnaire</a></li>
-		<li><a href="https://docs.google.com/forms/d/e/1FAIpQLSdEL_r30yB7v9OsfQNZJcspChBFOG2pE1d2K9zLfiAtkC-Vrg/viewform">Summer School Registration Form</a></li>
 	</ul>
 </div>
--- dms-child/template-parts/mega-menu-dropdowns-policies.php
+++ dms-child/template-parts/mega-menu-dropdowns-policies.php
@@ -33,16 +33,12 @@
 					<ul>
 						<li><a href="https://www.myschoolapps.com/">Application for Free and Reduced Meals</a></li>
 						<li><a href="https://docs.google.com/forms/d/e/1FAIpQLSdEL_r30yB7v9OsfQNZJcspChBFOG2pE1d2K9zLfiAtkC-Vrg/viewform">Summer School (JumpStart) Registration Form</a></li>
-						<li><a href="https://dixon.provo.edu/wp-content/uploads/2014/12/SCC-Nomination-Form-for-Election-2016-2017.pdf">School Community Council Candidate Form</a></li>
-						<li><a href="http://dixon.provo.edu/wp-content/uploads/2021/11/Job-Shadow-Agreement.pdf">Job Shadow Agreement Form</a></li>
-
 					</ul>
 
 				</div>
 				<div class="dropDownColumn noheading">
 					<ul>
-						<li><a href="https://dixon.provo.edu/wp-content/uploads/2021/02/2021-2022-Immunization-Requirements.pdf">2021-2022 Immunization Requirements Form</a></li>
-						<li><a href="https://dixon.provo.edu/wp-content/uploads/2021/02/2021-2022-Vacunas.pdf">2021-2022 Immunization Requirements Form (Spanish)</a></li>
-						<li><a href="http://dixon.provo.edu/wp-content/uploads/2015/02/Residency-Questionnaire.pdf">Student Residency Questionnaire</a></li>
+						<li><a href="https://dixon.provo.edu/wp-content/uploads/2014/12/SCC-Nomination-Form-for-Election-2016-2017.pdf">School Community Council Candidate Form</a></li>
+						<li><a href="http://dixon.provo.edu/wp-content/uploads/2021/11/Job-Shadow-Agreement.pdf">Job Shadow Agreement Form</a></li>
 					</ul>
 				</div>
